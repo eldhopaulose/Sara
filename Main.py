f7d5f4eda2c501b88e970ebe4523d8488bda92cf
Eldho Paulose <eldhopaulose0485@gmail.com>
2020-06-18 22:53:45 +0530

    Speech to text translation

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ import os
 import speech_recognition as sr
 import datetime
 def Text_audio(speak):
-    print(speak)
     tts = gTTS(text=speak, lang='en')
     tts.save("hello.mp3")
     os.system("mpg321 hello.mp3")
@@ -20,3 +19,20 @@ def wishMe():
 
     Text_audio("I am Sara Sir. Please tell me how may I help you")   
 wishMe()
+def myCommand():
+    Text_audio("Listening")                                                            
+    r = sr.Recognizer()                                                                                  
+    with sr.Microphone() as source: 
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)                                                                        
+    try: 
+        MyText = r.recognize_google(audio, language='en-in') 
+        MyText = MyText.lower() 
+        print("You said " + MyText)
+    except sr.UnknownValueError:
+        Text_audio("Could not understand audio")
+        print("Could not understand audio")
+        myCommand()
+myCommand()
